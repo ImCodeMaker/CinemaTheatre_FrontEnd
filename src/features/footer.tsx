4a60783c530d116c3d26173df23f9b196381e610
ImCodeMaker <xxjoshi2006@gmail.com>
2025-04-02 16:23:21 -0400

    Footer Skeleton, done

--- src/features/footer.tsx
+++ src/features/footer.tsx
@@ -1,19 +1,38 @@
 function Footer() {
     return (
         <>
-            <div className='bg-black h-[50dvh] w-[100dvw] flex justify-evenly items-center space-x-1'>
+            {/* Low key finishing all the landing page. Needs some improvement overall.  */}
+            <div className='bg-black h-[50dvh] w-[100dvw] flex justify-evenly items-center space-x-1 backdrop-blur-2xls'>
                <div className="flex flex-col space-y-2">
-                   <h1 className="text-white text-2xl">About us</h1>
-                   <span className="text-white font-semibold ">Our culture</span>
-                   <span className="text-white font-semibold">Our History</span>
-                   <span className="text-white font-semibold">Our Values</span>
-                   {/* Started this, forgot to finish it (as always) */}
+                   <h1 className="text-white text-3xl font-bold">Caribbean Cinemas</h1>
+                   <span className="text-white font-light hover:text-gray-400 cursor-pointer">About Us</span>
+                   <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our culture</span>
+                   <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our History</span>
+                   <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our Values</span>
+                   <span className="text-white font-light hover:text-gray-400 cursor-pointer">Terms of Services</span>
+                   <span className="text-white font-light hover:text-gray-400 cursor-pointer">Privacy and Security</span>
                </div>
                 <div>
-                    <h1 className="text-white text-2xl">About us</h1>
+                    <div className="flex flex-col space-y-2">
+                        <h1 className="text-white text-3xl font-bold">Caribbean Cinemas</h1>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">About Us</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our culture</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our History</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our Values</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Terms of Services</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Privacy and Security</span>
+                    </div>
                 </div>
                 <div>
-                    <h1 className="text-white text-2xl">About us</h1>
+                    <div className="flex flex-col space-y-2">
+                        <h1 className="text-white text-3xl font-bold">Caribbean Cinemas</h1>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">About Us</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our culture</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our History</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Our Values</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Terms of Services</span>
+                        <span className="text-white font-light hover:text-gray-400 cursor-pointer">Privacy and Security</span>
+                    </div>
                 </div>
             </div>
         </>
